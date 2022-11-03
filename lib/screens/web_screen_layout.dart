5d1914b187a98a7351c591adc4436cf707a35b3c
VTarunaKumar <vtarunakumarrao@gmail.com>
2022-11-03 14:31:42 +0530

    Completing WebChat Screen

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/widgets/chat_list.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
 
@@ -32,7 +33,64 @@ class WebScreenLayout extends StatelessWidget {
             child: Column(children: [
               WebAppChatBar(),
               Expanded(child: ChatList()),
-              //Message Input Box
+              Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: dividerColor,
+                      ),
+                    ),
+                    color: chatBarMessage),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.emoji_emotions_outlined,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.attach_file,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          right: 15,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              fillColor: searchBarColor,
+                              filled: true,
+                              hintText: "Type a message",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              contentPadding: EdgeInsets.only(left: 20)),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.mic,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ]),
           ),
         ],
